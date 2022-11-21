66b3a520b60be26756b97089db2be0737003a6d2
laugirard <lautaro.girard@gmail.com>
2022-11-21 11:08:28 -0300

    First commit

---
+++ Day_1_Exercises.js
@@ -0,0 +1,49 @@
+//1
+//comments can make code readable
+//2
+//Welcome to 30DaysOfJavaScript
+//3
+/*comments can make code readable, easy to reuse and informative*/
+//4
+//variable.js
+console.log("* File variables.js")
+let stringVar = "string"
+let booleanVar = true
+let undefinedVar
+let nullVar = null
+console.log(stringVar)
+console.log(booleanVar)
+console.log(undefinedVar)
+console.log(nullVar)
+console.log("--------------------")
+//5
+//datatypes.js
+console.log("* File datatypes.js")
+console.log(typeof stringVar)
+console.log(typeof booleanVar)
+console.log(typeof undefinedVar)
+console.log(typeof nullVar)
+console.log("--------------------")
+//6
+let var1
+let var2
+let var3
+let var4
+//7
+let varOne = 1
+let varTwo = 2
+let varThree = 3
+let varFour = 4
+//8
+let firstName = 'Lautaro'
+let lastName = 'Girard'
+let maritalStatus = 'Single'
+let country = 'Argentina'
+let age = 31
+//9
+let firstName2 = 'Lautaro', lastName2 = 'Girard', maritalStatus2 = 'Single', country2 = 'Argentina', age2 = 31
+//10
+let myAge = 31
+let yourAge = 31
+console.log('I am ' + myAge + ' years old.')
+console.log('You are ' + yourAge + ' years old.')
